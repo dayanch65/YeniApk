cd4cf2bb19f5f56872dd3c211a163bfc5dcbe1e0
dayanch65 <dhojayew498@gmail.com>
2026-06-22 19:11:20 +0500

    Bgb

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,48 @@
-import 'package:flutter/material.dart';
-import 'package:dropdown_search/dropdown_search.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('DropdownSearch Örneği')),
-        body: const Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Center(
-            child: ColorDropdown(),
-          ),
+DropdownSearch<String>(
+  items: (filter, infiniteScrollProps) => ['Item 1', 'Item 2', 'Item 3'],
+  suffixProps: DropdownSuffixProps(
+    dropdownButtonProps: DropdownButtonProps(
+      iconClosed: Icon(Icons.keyboard_arrow_down),
+      iconOpened: Icon(Icons.keyboard_arrow_up),
+    ),
+  ),
+  decoratorProps: DropDownDecoratorProps(
+    textAlign: TextAlign.center,
+    decoration: InputDecoration(
+      contentPadding: EdgeInsets.symmetric(vertical: 20),
+      filled: true,
+      fillColor: Colors.white,
+      border: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      hintText: 'Please select...',
+      hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.grey),
+    ),
+  ),
+  popupProps: PopupProps.menu(
+    itemBuilder: (context, item, isDisabled, isSelected) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
+        child: Text(
+          item,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          textAlign: TextAlign.center,
         ),
-      ),
-    );
-  }
-}
-
-class ColorItem {
-  final String name;
-  final Color color;
-  ColorItem(this.name, this.color);
-
-  @override
-  String toString() => name;
-}
-
-class ColorDropdown extends StatelessWidget {
-  const ColorDropdown({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final items = <ColorItem>[
-      ColorItem("Red", Colors.red),
-      ColorItem("Black", Colors.black),
-      ColorItem("Yellow", Colors.yellow),
-      ColorItem("Blue", Colors.blue),
-    ];
-
-    return DropdownSearch<ColorItem>(
-      items: items,
-      popupProps: PopupProps.menu(
-        showSearchBox: true,
-        itemBuilder: (context, item, isSelected) => Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            children: [
-              Container(
-                width: 20,
-                height: 20,
-                decoration: BoxDecoration(
-                  color: item?.color ?? Colors.transparent,
-                  shape: BoxShape.circle,
-                ),
-              ),
-              const SizedBox(width: 12),
-              Text(
-                item?.name ?? '',
-                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              ),
-            ],
-          ),
-        ),
-      ),
-      compareFn: (item1, item2) => item1?.name == item2?.name,
-      dropdownBuilder: (ctx, selectedItem) => Icon(
-        Icons.face,
-        color: selectedItem?.color ?? Colors.grey,
-        size: 54,
-      ),
-      onChanged: (v) {},
-      // Eğer seçili öğeyi yazdırmak isterseniz itemAsString da ekleyebilirsiniz:
-      // itemAsString: (ColorItem? c) => c?.name ?? '',
-    );
-  }
-}
+      );
+    },
+    constraints: BoxConstraints(maxHeight: 160),
+    menuProps: MenuProps(
+      margin: EdgeInsets.only(top: 12),
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+    ),
+  ),
+),
