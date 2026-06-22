50a4984d527c42a3a4742421cdbcd256b810b559
dayanch65 <dhojayew498@gmail.com>
2026-06-22 15:17:35 +0500

    Hh

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 DropdownSearch<(String, Color)>(
   clickProps: ClickProps(borderRadius: BorderRadius.circular(20)),
-  mode: Mode.CUSTOM,
-  items: (f, cs) => [
+  mode: Mode.menu,
+  items: (filter, loadProps) => [
     ("Red", Colors.red),
     ("Black", Colors.black),
     ("Yellow", Colors.yellow),
-    ('Blue', Colors.blue),
+    ("Blue", Colors.blue),
   ],
   compareFn: (item1, item2) => item1.$1 == item2.$1,
   popupProps: PopupProps.menu(
-  menuProps: MenuProps(align: MenuAlign.bottomCenter),
+    menuProps: MenuProps(align: MenuAlign.bottom),
     fit: FlexFit.loose,
     itemBuilder: (context, item, isDisabled, isSelected) => Padding(
       padding: const EdgeInsets.all(8.0),
       child: Text(item.$1, style: TextStyle(color: item.$2, fontSize: 16)),
     ),
   ),
-  dropdownBuilder: (ctx, selectedItem) => Icon(Icons.face, color: selectedItem?.$2, size: 54),
-),
+  dropdownBuilder: (ctx, selectedItem) => Icon(
+    Icons.face,
+    color: selectedItem?.$2 ?? Colors.grey,
+    size: 54,
+  ),
+)
