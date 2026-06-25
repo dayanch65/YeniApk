f5f24c83102f0867c6af6862f9be78259448ba5e
dayanch65 <dhojayew498@gmail.com>
2026-06-25 15:23:26 +0500

    Jv

---
+++ Main2.dart
@@ -0,0 +1,29 @@
+// kolay_flutter.dart
+import 'package:flutter/material.dart';
+
+// 1. Başlatıcı Fonksiyon
+final uygulamayiBaslat = runApp;
+
+// 2. Temel Yapı Taşları ve Tipler
+typedef Bilesen = Widget;
+typedef Baglam = BuildContext;
+typedef SabitTasarim = StatelessWidget;
+
+// 3. Uygulama ve Renk Ayarları
+typedef AnaUygulama = MaterialApp;
+typedef TemaVerisi = ThemeData;
+typedef Renkler = Colors;
+
+// 4. Ekran Düzeni Elemanları
+typedef Iskelet = Scaffold;
+typedef UstBar = AppBar;
+typedef Liste = ListView;
+typedef ListeElemani = ListTile;
+
+// 5. Görsel ve İçerik Elemanları
+typedef Yazi = Text;
+typedef YaziStili = TextStyle;
+typedef YuvarlakProfil = CircleAvatar;
+typedef Simge = Icon;
+typedef Simgeler = Icons;
+typedef YuvarlakButon = FloatingActionButton;
